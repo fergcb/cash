568ba9c93fd8387f8f5991dc6a018d5721390d36
fergcb <fergus.bentley@gmail.com>
2022-07-29 22:23:59 +0100

    Add skeleton for full cli functionality

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	// inst "cash/instruction"
-	// "cash/machine"
 	"bufio"
 	"cash/error"
 	"cash/machine"
@@ -13,23 +11,59 @@ import (
 	"github.com/alexflint/go-arg"
 )
 
+type CompileCommand struct {
+	Input string `arg:"positional"`
+}
+
+type AssembleCommand struct {
+	Input string `arg:"positional"`
+}
+
+type RunCommand struct {
+	Input string `arg:"positional"`
+}
+
 func main() {
+
 	var args struct {
-		Input string `arg:"positional"`
+		Compile  *CompileCommand  `arg:"subcommand:compile"`
+		Assemble *AssembleCommand `arg:"subcommand:assemble"`
+		Run      *RunCommand      `arg:"subcommand:run"`
 	}
 
 	arg.MustParse(&args)
-
-	if strings.HasSuffix(args.Input, ".cm") {
-		runMachineCode(args.Input)
+	if args.Compile != nil {
+		compile(args.Compile)
+	} else if args.Assemble != nil {
+		assemble(args.Assemble)
+	} else if args.Run != nil {
+		run(args.Run)
 	}
 }
 
-func runMachineCode(inputPath string) {
+func compile(args *CompileCommand) {
+	panic("function not implemented")
+}
+
+func assemble(args *AssembleCommand) {
+	panic("function not implemented")
+}
+
+func run(args *RunCommand) {
+	inputPath := args.Input
+
 	file, err := os.Open(inputPath)
 	error.Check(err)
 	defer file.Close()
 
+	if strings.HasSuffix(inputPath, ".cash") {
+		runCashCodeFromFile(file)
+	} else if strings.HasSuffix(inputPath, ".cm") {
+		runMachineCodeFromFile(file)
+	}
+}
+
+func runMachineCodeFromFile(file *os.File) {
 	scanner := bufio.NewScanner(file)
 
 	program, err := machinecode.Parse(scanner)
@@ -41,4 +75,8 @@ func runMachineCode(inputPath string) {
 
 	err = scanner.Err()
 	error.Check(err)
+}
+
+func runCashCodeFromFile(file *os.File) {
+	panic("function not implemented")
 }
